4b73c90b48607e70c8b12ee0b0a3d0e2b30909b5
eieio81810 <34175263+eieio81810@users.noreply.github.com>
2023-09-21 00:19:35 +0900

    StoryBookの作成

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,115 +1,177 @@
 import 'package:flutter/material.dart';
+import 'package:storybook_flutter/storybook_flutter.dart';
+//import 'package:storybook_flutter_example/router_aware_stories.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  Widget build(BuildContext context) => Storybook(
+        initialStory: 'Screens/Scaffold',
+        stories: [
+          //...routerAwareStories,
+          Story(
+            name: 'Screens/Scaffold',
+            description: 'Story with scaffold and different knobs.',
+            builder: (context) => Scaffold(
+              appBar: AppBar(
+                title: Text(
+                  context.knobs.text(
+                    label: 'Title',
+                    initial: 'Scaffold',
+                    description: 'The title of the app bar.',
+                  ),
+                ),
+                elevation: context.knobs.nullable.slider(
+                  label: 'AppBar elevation',
+                  initial: 4,
+                  min: 0,
+                  max: 10,
+                  description: 'Elevation of the app bar.',
+                ),
+                backgroundColor: context.knobs.nullable.options(
+                  label: 'AppBar color',
+                  initial: Colors.blue,
+                  description: 'Background color of the app bar.',
+                  options: const [
+                    Option(
+                      label: 'Blue',
+                      value: Colors.blue,
+                      description: 'Blue color',
+                    ),
+                    Option(
+                      label: 'Green',
+                      value: Colors.green,
+                      description: 'Green color',
+                    ),
+                  ],
+                ),
+              ),
+              body: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: List.generate(
+                    context.knobs.sliderInt(
+                      label: 'Items count',
+                      initial: 2,
+                      min: 1,
+                      max: 5,
+                      description: 'Number of items in the body container.',
+                    ),
+                    (_) => const Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Text('Hello World!'),
+                    ),
+                  ),
+                ),
+              ),
+              floatingActionButton: context.knobs.boolean(
+                label: 'FAB',
+                initial: true,
+                description: 'Show FAB button',
+              )
+                  ? FloatingActionButton(
+                      onPressed: () {},
+                      child: const Icon(Icons.add),
+                    )
+                  : null,
+            ),
+          ),
+          Story(
+            name: 'Screens/Counter',
+            description: 'Demo Counter app with about dialog.',
+            builder: (context) => CounterPage(
+              title: context.knobs.text(label: 'Title', initial: 'Counter'),
+              enabled: context.knobs.boolean(label: 'Enabled', initial: true),
+            ),
+          ),
+          Story(
+            name: 'Widgets/Text',
+            description: 'Simple text widget.',
+            builder: (context) => const Center(child: Text('Simple text')),
+          ),
+          Story(
+            name: 'Story/Nested/Multiple/Times/First',
+            builder: (context) => const Center(child: Text('First')),
+          ),
+          Story(
+            name: 'Story/Nested/Multiple/Times/Second',
+            builder: (context) => const Center(child: Text('Second')),
+          ),
+          Story(
+            name: 'Story/Nested/Multiple/Third',
+            builder: (context) => const Center(child: Text('Third')),
+          ),
+          Story(
+            name: 'Story/Nested/Multiple/Fourth',
+            builder: (context) => const Center(child: Text('Fourth')),
+          ),
+          Story(
+            name: 'Story without a category',
+            builder: (context) => const Center(child: Text('Simple text')),
+          ),
+        ],
+      );
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class CounterPage extends StatefulWidget {
+  const CounterPage({
+    Key? key,
+    required this.title,
+    this.enabled = true,
+  }) : super(key: key);
 
   final String title;
+  final bool enabled;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CounterPage> createState() => _CounterPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _CounterPageState extends State<CounterPage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  void _incrementCounter() => setState(() => _counter++);
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.help),
+              onPressed: () => showAboutDialog(
+                context: context,
+                applicationName: 'Storybook',
+                applicationVersion: '0.0.1',
+                applicationIcon: const Icon(Icons.book),
+                applicationLegalese: 'MIT License',
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('You have pushed the button this many times:'),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: widget.enabled
+            ? FloatingActionButton(
+                onPressed: _incrementCounter,
+                tooltip: 'Increment',
+                child: const Icon(Icons.add),
+              )
+            : null,
+      );
 }
